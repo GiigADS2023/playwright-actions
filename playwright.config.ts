afb994c19e8080a51b6f79e972a491c5cbda132f
Giorgia Batista Schmidt <126842688+GiigADS2023@users.noreply.github.com>
2026-06-18 21:55:38 -0300

    Testes multi-browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: 'http://loginxp.vercel.app',
+    video: 'on', // Grava um vídeo para cada teste, mesmo os que passam. O vídeo é salvo na pasta de resultados do teste.
   },
 
   /* Configure projects for major browsers */
@@ -40,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
